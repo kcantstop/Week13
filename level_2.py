c13ddefb88a770c695ed73fa6a27f5a68ee82ab4
Keaton Caldwell <keatoncaldwell@Keatons-MacBook-Pro.local>
2025-04-24 08:41:20 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,9 @@
+mix_of_stuff = [5, "happy", True, 53.35]
+
+playlist_for_coding = ["Persian Rug", "People Tell Me", "Do I ever Cross Your Mind", "Ramblin", "Memory"]
+
+
+playlist_for_coding.append("Never Gonna Give You Up")
+
+print(playlist_for_coding[3])
+
